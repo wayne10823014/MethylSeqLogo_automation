54d4d6fbd6e1b4d6e8aa441ed952139cefcfa672
Wayne <514041stu@gmail.com>
2024-11-04 16:52:22 +0800

    LogoJS能生成固定資料的圖片

--- bin/main.py
+++ bin/main.py
@@ -6,12 +6,13 @@ import numpy as np
 import matplotlib
 import sys
 import unittest
+import json  # 新增
 
 # matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.patheffects
 from package.arg import get_parser
-from package.datapreprocess import  get_tfbs,get_seq, methylread_counter, flanking_bed
+from package.datapreprocess import get_tfbs, get_seq, methylread_counter, flanking_bed
 from package.backgroundprocess import read_bgprob_table, promoter, isfile, neighbor
 from package.calculate import *
 from package.figure_setting import *
@@ -20,26 +21,28 @@ import time
 from Bio import SeqIO
 from Bio.SeqUtils.CheckSum import seguid
 
+
 def remove_dup_seqs(records):
     """"SeqRecord iterator to removing duplicate sequences."""
     checksums = set()
     for record in records:
         checksum = seguid(record.seq)
         if checksum in checksums:
-            # print ("Ignoring %s" % record.id)
+            # print("Ignoring %s" % record.id)
             continue
         checksums.add(checksum)
         yield record
 
+
 def main():
-    filestart= time.time()
-    # logging.basicConfig(filename="methylseqlogo.log", level = logging.DEBUG, format = '%(asctime)s, %(name)s-%(levelname)-s: %(message)s')
+    filestart = time.time()
+    # logging.basicConfig(filename="methylseqlogo.log", level=logging.DEBUG, format='%(asctime)s, %(name)s-%(levelname)-s: %(message)s')
     parser = get_parser()
     args = parser.parse_args()
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    TF = args.transcriptionfactor  
+    TF = args.transcriptionfactor
     species = args.species
     celltype = args.celltypes
     mode = args.mode
@@ -51,45 +54,40 @@ def main():
     plotlen = args.plotlen
 
     methylbed = [args.mcg, args.mchg, args.mchh]
-  
+
     pd.set_option('display.float_format', lambda x: '%.2f' % x)
 
-
-    print ("\n")
+    print("\n")
     if logotype in ['Shannon', 'Kullback-Liebler']:
-        print ("Plotting " + TF + ' ' + mode + ' ' + logotype + ' logo of ' + species + ' ' + celltype + ' ' + region + ' background.')
+        print("Plotting " + TF + ' ' + mode + ' ' + logotype + ' logo of ' + species + ' ' + celltype + ' ' + region + ' background.')
     elif logotype == 'riverlake':
-        print ("Plotting " + TF + ' ' + mode + ' ' + logotype + ' of ' + species + ' ' + celltype + '.')
+        print("Plotting " + TF + ' ' + mode + ' ' + logotype + ' of ' + species + ' ' + celltype + '.')
     else:
-        print ("Plotting " + TF + ' ' + mode + ' all logos of ' + species + ' ' + celltype + '.')
-    print ("\n")
-    
+        print("Plotting " + TF + ' ' + mode + ' all logos of ' + species + ' ' + celltype + '.')
+    print("\n")
 
-
-    # span = 50 if region == "promoter" else 2 
-    spanL = int(region) if region.isdigit()  else 2
+    # span = 50 if region == "promoter" else 2
+    spanL = int(region) if region.isdigit() else 2
     spanR = spanL
 
-    jaspar_file= args.jaspar
+    jaspar_file = args.jaspar
     remap_file = args.remap
     tfbs_bed, tfbs = get_tfbs(remap_file, jaspar_file, species, spanL, spanR)
 
+    seqdata = get_seq(species, tfbs[0], tfbs[1], spanL, spanR)
 
-    
-    seqdata = get_seq(species, tfbs[0], tfbs[1],spanL, spanR)
-    
     try:
         seqdata = SeqIO.to_dict(SeqIO.parse(seqdata, 'fasta'))
     except:
         # print("SeqRecord iterator to removing duplicate sequences.")
         seqdata = remove_dup_seqs(SeqIO.parse(seqdata, 'fasta'))
         seqdata = SeqIO.to_dict(seqdata)
-    
-    seqdata= pd.DataFrame.from_dict(seqdata, orient= 'index')
-    
-    seqdata.reset_index(drop= True)
-    # seqdata.to_csv( 'seq.csv')
-    print(len(seqdata))  
+
+    seqdata = pd.DataFrame.from_dict(seqdata, orient='index')
+
+    seqdata.reset_index(drop=True)
+    # seqdata.to_csv('seq.csv')
+    print(len(seqdata))
     start = time.time()
 
     output_dir = "/home/wayne/MethylSeqLogo_automation/Output1/"
@@ -102,20 +100,19 @@ def main():
 
     print(ctx_file)
 
-    # 檢查文件是否存在
+    # 检查文件是否存在
     if os.path.isfile(ctx_file) and os.path.isfile(cread_file) and os.path.isfile(tread_file):
         ctxdata = pd.read_csv(ctx_file, sep='\t')
         creaddata = pd.read_csv(cread_file, sep='\t')
         treaddata = pd.read_csv(tread_file, sep='\t')
     else:
         ctxdata, creaddata, treaddata = methylread_counter(tfbs_bed, methylbed)
-    
+
     end = time.time()
-    print('\nmethylatedread_counter finished...,total cost', (end-start)//60, 'min\n')
+    print('\nmethylatedread_counter finished...,total cost', (end - start) // 60, 'min\n')
 
-
-    motif_len = len(seqdata.columns) - (spanL+spanR)
-    print (TF + " binding motif is " + str(motif_len) + "bp")
+    motif_len = len(seqdata.columns) - (spanL + spanR)
+    print(TF + " binding motif is " + str(motif_len) + "bp")
 
     if plotlen is None:
         plotlen = motif_len - beginningoftfbs + 1
@@ -125,37 +122,36 @@ def main():
     global endpos
     if (beginningoftfbs + plotlen > motif_len):
         endpos = motif_len
-        print ("Warning: user defined plotting length is over motif length" + '\n')
-        print ("Plotting " + TF + " binding motif from pos " + str(beginningoftfbs) + " to pos " + str(motif_len))
+        print("Warning: user defined plotting length is over motif length" + '\n')
+        print("Plotting " + TF + " binding motif from pos " + str(beginningoftfbs) + " to pos " + str(motif_len))
     else:
         endpos = beginningoftfbs + plotlen - 1
-        print ("Plotting " + TF + " binding motif from pos " + str(beginningoftfbs) + " to pos " + str(endpos))
+        print("Plotting " + TF + " binding motif from pos " + str(beginningoftfbs) + " to pos " + str(endpos))
 
-  
     print("\n")
-    seqdata = seqdata.iloc[:, beginningoftfbs - 1 + spanL : beginningoftfbs - 1 + spanL + motif_len]
+    seqdata = seqdata.iloc[:, beginningoftfbs - 1 + spanL: beginningoftfbs - 1 + spanL + motif_len]
     # print(seqdata)
     ctxdata = ctxdata.iloc[:, beginningoftfbs - 1:endpos]
     # print(ctxdata)
-    creaddata = creaddata.iloc[:, beginningoftfbs - 1 : endpos]
-    treaddata = treaddata.iloc[:, beginningoftfbs - 1 : endpos]
+    creaddata = creaddata.iloc[:, beginningoftfbs - 1: endpos]
+    treaddata = treaddata.iloc[:, beginningoftfbs - 1: endpos]
     # print(creaddata)
     # print(treaddata)
 
-    output_dir = "/home/wayne/MethylSeqLogo_automation/Output1"
+    output_dir = os.path.dirname(os.path.abspath(__file__))  # 修改为当前脚本所在目录
 
-    # 確保目錄存在，如果不存在則創建
+    # 确保目录存在，如果不存在则创建
     os.makedirs(output_dir, exist_ok=True)
 
-    # 文件名的前綴
+    # 文件名的前缀
     logoname = TF + '_' + species + '_' + celltype + '_' + region + '_' + mode + '_' + logotype + '_'
 
-    # 使用 os.path.join 將路徑和文件名連接起來
+    # 使用 os.path.join 将路径和文件名连接起来
     ctx_filepath = os.path.join(output_dir, logoname + "ctx.csv")
     cread_filepath = os.path.join(output_dir, logoname + "cread.csv")
     tread_filepath = os.path.join(output_dir, logoname + "tread.csv")
 
-    # 將數據存成 CSV 文件
+    # 将数据存成 CSV 文件
     ctxdata.to_csv(ctx_filepath, sep='\t', index=False)
     creaddata.to_csv(cread_filepath, sep='\t', index=False)
     treaddata.to_csv(tread_filepath, sep='\t', index=False)
@@ -163,66 +159,80 @@ def main():
     global pseudocount
     pseudocount = 1.0
 
-      
-    #Check species.fa exits
-    isfile(dir_path + '/genome/' + species +'.fa')
+    # Check species.fa exists
+    isfile(dir_path + '/genome/' + species + '.fa')
 
     if region == 'whole_genome':
         bgpps, bg_mCG, bg_mCHG, bg_mCHH = read_bgprob_table(species, celltype, region)
     elif (region.isdigit()):
-       print('~~neighbor~~')
-       path = dir_path + "/../Background_probability/neighbor/"+ species + '_' + TF  +'_'+ celltype + '_' + region + '_probability.txt'
-       path1 = dir_path + "/../Background_probability/neighbor/" + species + '_' + TF + '_' + celltype + '_' + region  +'_methyl_probability.txt'
-       if os.path.isfile(path) and os.path.isfile(path1):
+        print('~~neighbor~~')
+        path = dir_path + "/../Background_probability/neighbor/" + species + '_' + TF + '_' + celltype + '_' + region + '_probability.txt'
+        path1 = dir_path + "/../Background_probability/neighbor/" + species + '_' + TF + '_' + celltype + '_' + region + '_methyl_probability.txt'
+        if os.path.isfile(path) and os.path.isfile(path1):
             print('~~find neighbor~~')
-            bgpps,  bg_mCG, bg_mCHG, bg_mCHH = read_bgprob_table(species, celltype, region, TF)
-       else:
+            bgpps, bg_mCG, bg_mCHG, bg_mCHH = read_bgprob_table(species, celltype, region, TF)
+        else:
             print('~~not find neighbor~~')
             flankingbed = flanking_bed(tfbs_bed, spanL, spanR)
-            bgpps,  bg_mCG, bg_mCHG, bg_mCHH =  neighbor(flankingbed, species, methylbed, celltype, region, TF)
+            bgpps, bg_mCG, bg_mCHG, bg_mCHH = neighbor(flankingbed, species, methylbed, celltype, region, TF)
 
     else:
-       print('~~promoter~~')
-       start = time.time()
-       path = dir_path + "/../Background_probability/promoter/"+ species + '_' + celltype + '_' + region + '_probability.txt'
-       path1 = dir_path + "/../Background_probability/promoter/"+ species + '_' + celltype + '_' + region +'_methyl_probability.txt'
-       if os.path.isfile(path) and os.path.isfile(path1):
+        print('~~promoter~~')
+        start = time.time()
+        path = dir_path + "/../Background_probability/promoter/" + species + '_' + celltype + '_' + region + '_probability.txt'
+        path1 = dir_path + "/../Background_probability/promoter/" + species + '_' + celltype + '_' + region + '_methyl_probability.txt'
+        if os.path.isfile(path) and os.path.isfile(path1):
             print('~~find promoter~~')
-            bgpps,  bg_mCG, bg_mCHG, bg_mCHH = read_bgprob_table(species, celltype, region)
-       else:
+            bgpps, bg_mCG, bg_mCHG, bg_mCHH = read_bgprob_table(species, celltype, region)
+        else:
             print('~~not find promoter~~')
-            bgpps,  bg_mCG, bg_mCHG, bg_mCHH =  promoter(tfbs_bed, species, methylbed, celltype, region)
+            bgpps, bg_mCG, bg_mCHG, bg_mCHH = promoter(tfbs_bed, species, methylbed, celltype, region)
             end = time.time()
-            print('promoter bg calc finished...,total cost', (end-start)//60, 'min\n')
+            print('promoter bg calc finished...,total cost', (end - start) // 60, 'min\n')
 
+    ppm = calc_ppm(seqdata, TF, species, celltype, region)
 
-    ppm = calc_ppm(seqdata, TF, species, celltype, region,)
+    # 新增代码：将 PPM 写入 JSON 文件到与 home.html 相同的目录
+    output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'package')
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    ppm_filepath = os.path.join(output_dir, 'ppm.json')
+
+    ppm_list = ppm[['A', 'C', 'G', 'T']].values.tolist()
+    with open(ppm_filepath, 'w') as f:
+        json.dump(ppm_list, f)
+    print("PPM 数据已保存到 ppm.json 文件中，路径为：", ppm_filepath)
+
     C_ratio, G_ratio, Cmethyls, Gmethyls, Freqs_ = calc_methylprob(ctxdata, creaddata, treaddata, bg_mCG, bg_mCHG, bg_mCHH, plotlen)
 
     if logotype in ['Kullback-Liebler', 'Shannon']:
-        Cents = calc_methylation_entropy( C_ratio, G_ratio, Cmethyls, Gmethyls, bg_mCG, bg_mCHG, bg_mCHH, logotype)
+        Cents = calc_methylation_entropy(C_ratio, G_ratio, Cmethyls, Gmethyls, bg_mCG, bg_mCHG, bg_mCHH, logotype)
         entropys = calc_totalEntropy(ppm, bgpps, Cents, logotype, plotlen, TF, species, celltype, region)
         four_base_heights = calc_perBaseEntropy(entropys, ppm, mode, TF, species, celltype, region)
         dippm = to4basedippm(seqdata, plotlen)
         dientropys, bg_dientropys_max, bg_dientropys_min = twomerBg(bgpps, dippm, plotlen)
         fig = set_fig(entropys, logotype, mode, plotlen)
-        plotobj = seqLogoPlot(fig, celltype, four_base_heights, entropys, Cmethyls, Gmethyls, bgpps, dientropys, bg_dientropys_max, bg_dientropys_min, bg_mCG, bg_mCHG, bg_mCHH, Freqs_, mode, plotlen, threshold, TF)
+        plotobj = seqLogoPlot(fig, celltype, four_base_heights, entropys, Cmethyls, Gmethyls, bgpps, dientropys,
+                              bg_dientropys_max, bg_dientropys_min, bg_mCG, bg_mCHG, bg_mCHH, Freqs_, mode, plotlen,
+                              threshold, TF)
         plotobj.plotlogo()
         logoname = TF + '_' + species + '_' + celltype + '_' + region + '_' + mode + '_' + logotype + '_seqlogo.png'
-        plt.savefig(dir_path + '/../Output1/' + logoname, bbox_inches = 'tight', dpi = 600)
-        print (logoname + ' is saved in' + dir_path + '/../Output1/.')
+        plt.savefig(dir_path + '/../Output1/' + logoname, bbox_inches='tight', dpi=600)
+        print(logoname + ' is saved in' + dir_path + '/../Output1/.')
     elif logotype == 'riverlake':
         dippm = to4basedippm(seqdata)
         Cents = calc_methylation_entropy(C_ratio, G_ratio, bg_mCG, bg_mCHG, bg_mCHH, logotype)
         entropys = calc_totalEntropy(ppm, bgpps, Cents, logotype)
         four_base_heights = calc_perBaseEntropy(entropys, ppm)
         # fig = set_fig(entropys)
-        fig = plt.figure(figsize = (plotlen+1, 3.0))
-        riverlakeobj = riverLake(fig, celltype, ppm, dippm, Cmethyls, Gmethyls, bgpps, bg_mCG, bg_mCHG, bg_mCHH, Freqs_)
+        fig = plt.figure(figsize=(plotlen + 1, 3.0))
+        riverlakeobj = riverLake(fig, celltype, ppm, dippm, Cmethyls, Gmethyls, bgpps, bg_mCG, bg_mCHG, bg_mCHH,
+                                 Freqs_, mode, plotlen, TF, motif_len)
         riverlakeobj.plotRiverLake()
         logoname = TF + '_' + species + '_' + celltype + '_' + region + '_' + mode + '_' + logotype + '_seqlogo_bar7.pdf'
-        plt.savefig(dir_path + '/../Output1/' + logoname, bbox_inches = 'tight', dpi = 600)
-        print (logoname + ' is saved in' + dir_path + '/../Output1/.')
+        plt.savefig(dir_path + '/../Output1/' + logoname, bbox_inches='tight', dpi=600)
+        print(logoname + ' is saved in' + dir_path + '/../Output1/.')
     elif logotype == 'all':
         for i in ['Kullback-Liebler', 'Shannon']:
             # Cents = methylationEntropy(JiCs, PiCs, J_bCG, J_bCHG, J_bCHH, logotype)
@@ -231,11 +241,12 @@ def main():
             # entropys = totalEntropy(ppm, bgpps, Cents, logotype)
             four_base_heights = calc_perBaseEntropy(entropys, ppm)
             fig = set_fig(entropys)
-            plotobj = seqLogoPlot(fig, celltype, four_base_heights, entropys, Cmethyls, Gmethyls, bgpps, bg_mCG, bg_mCHG, bg_mCHH, Freqs_)
+            plotobj = seqLogoPlot(fig, celltype, four_base_heights, entropys, Cmethyls, Gmethyls, bgpps, bg_mCG,
+                                  bg_mCHG, bg_mCHH, Freqs_)
             plotobj.plotlogo()
             logoname = TF + '_' + species + '_' + celltype + '_' + region + '_' + mode + '_' + i + '_seqlogo_bar7.pdf'
-            plt.savefig(dir_path + '/../Output1/' + logoname, bbox_inches = 'tight', dpi = 600)
-            print (logoname + ' is saved in ./Output1/.')
+            plt.savefig(dir_path + '/../Output1/' + logoname, bbox_inches='tight', dpi=600)
+            print(logoname + ' is saved in ./Output1/.')
         dippm = to4basedippm(seqdata)
         dientropys = twomerBg(bgpps, dippm)
         Cents = calc_methylation_entropy(C_ratio, G_ratio, bg_mCG, bg_mCHG, bg_mCHH, 'riverlake')
@@ -243,49 +254,47 @@ def main():
         four_base_heights = calc_perBaseEntropy(entropys, ppm)
         # fig = set_fig(entropys)
 
-        fig = plt.figure(figsize = (plotlen, 3.0))
-        riverlakeobj = riverLake(fig, celltype, ppm, dippm, Cmethyls, Gmethyls, bgpps, bg_mCG, bg_mCHG, bg_mCHH, Freqs_)
+        fig = plt.figure(figsize=(plotlen, 3.0))
+        riverlakeobj = riverLake(fig, celltype, ppm, dippm, Cmethyls, Gmethyls, bgpps, bg_mCG, bg_mCHG, bg_mCHH,
+                                 Freqs_)
         riverlakeobj.plotRiverLake()
         logoname = TF + '_' + species + '_' + celltype + '_' + region + '_' + mode + '_' + 'riverlake' + '_seqlogo_bar7.png'
-        plt.savefig(dir_path + '/../Output1/' + logoname, bbox_inches = 'tight', dpi = 600)
-        print (logoname + ' is saved in ./Output1/.')	
+        plt.savefig(dir_path + '/../Output1/' + logoname, bbox_inches='tight', dpi=600)
+        print(logoname + ' is saved in ./Output1/.')
     fileend = time.time()
 
     total_time = fileend - filestart
     minutes = total_time // 60
     seconds = round(total_time % 60)
     print(f"Total cost: {int(minutes)} min {seconds} sec")
-    print ("\n")
+    print("\n")
 
+    print("正在运行测试以验证输出是否正确...")
+    # 添加测试代码
+    # 确保 test_methylseqlogo_outputs.py 可以被导入
+    # 如果在 package 目录下，使用 from package import test_methylseqlogo_outputs
 
-    print("正在運行測試以驗證輸出是否正確...")
-    # 添加測試代碼
-    # 確保 test_methylseqlogo_outputs.py 可以被導入
-    # 如果在 package 目錄下，使用 from package import test_methylseqlogo_outputs
-
-    # 導入測試模塊
+    # 导入测试模块
     try:
         from package import test_methylseqlogo_outputs
     except ImportError:
         import test_methylseqlogo_outputs
 
-    # 創建測試加載器和測試套件
+    # 创建测试加载器和测试套件
     loader = unittest.TestLoader()
     suite = loader.loadTestsFromModule(test_methylseqlogo_outputs)
 
-    # 運行測試
+    # 运行测试
     runner = unittest.TextTestRunner()
     result = runner.run(suite)
 
-    # 檢查測試結果
+    # 检查测试结果
     if not result.wasSuccessful():
-        print("測試未通過，輸出存在錯誤。")
-        sys.exit(1)  # 退出程序，返回錯誤碼
+        print("测试未通过，输出存在错误。")
+        sys.exit(1)  # 退出程序，返回错误码
     else:
-        print("所有測試通過，輸出正確。")
+        print("所有测试通过，输出正确。")
 
 
-
-# main()
 if __name__ == '__main__':
     main()
