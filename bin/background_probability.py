56ad1ffd31f21fac3a07c01d28da8a185f8d2f51
Wayne <514041stu@gmail.com>
2024-11-13 14:56:18 +0800

    重構 backgroud_probability.py

--- bin/background_probability.py
+++ bin/background_probability.py
@@ -1,268 +1,277 @@
 import argparse
 import time
-from multiprocessing import Pool
-import multiprocessing as mp
+from multiprocessing import Pool, cpu_count
 from functools import reduce
 import numpy as np
 import os
 import itertools
 import pandas as pd
 
-'計算humun的 cpg, chg, chh, A, C, G, T,AA, AC..TT的比例'
-'有用Multiproccess加速從 20 -> 10 min'
-'只計算whole genome'
-
 def get_parser():
-    parser = argparse.ArgumentParser('background_probability')
-    parser.add_argument('-c', '--celltype', choices= ['H1-hESC', 'HepG2', 'K562', 'GM12878', 'HeLa-S3', 'naive-hESC', 'primed-hESC', 'mESC', 'mMPGC_E13p5', 'mMPGC_E16p5', 'mFPGC_E13p5', 'mFPGC_E16p5', 'Leaf', 'Inflorescence', 'Root', 'shoot'], default= 'HepG2', help= 'What type of cells is the WGBS_file from? ', metavar= 'cell types')    
-    parser.add_argument('-s', '--speciesname', help= 'species of the input data for MethylSeqLogo', metavar= 'species', default= 'human')
-    parser.add_argument('-fa', '--fasta', help= 'fasta of species', metavar= 'species', default= '/genome/human.fa')
+    """
+    创建命令行参数解析器。
+    """
+    parser = argparse.ArgumentParser(description='背景概率计算程序')
+    parser.add_argument(
+        '-c', '--celltype',
+        choices=[
+            'H1-hESC', 'HepG2', 'K562', 'GM12878', 'HeLa-S3', 'naive-hESC', 'primed-hESC',
+            'mESC', 'mMPGC_E13p5', 'mMPGC_E16p5', 'mFPGC_E13p5', 'mFPGC_E16p5',
+            'Leaf', 'Inflorescence', 'Root', 'shoot'
+        ],
+        default='HepG2',
+        metavar='cell types',
+        help='WGBS 文件所属的细胞类型'
+    )
+    parser.add_argument(
+        '-s', '--speciesname',
+        default='human',
+        metavar='species',
+        help='输入数据的物种名称，用于 MethylSeqLogo'
+    )
+    parser.add_argument(
+        '-fa', '--fasta',
+        default='/genome/human.fa',
+        metavar='species',
+        help='物种的 FASTA 文件路径'
+    )
 
-    
-    wgbs = parser.add_argument_group('arg with methylation calculation')
-    wgbs.add_argument('-CG','--mcg', nargs='+', type=str, help= 'WGBS at CpG')
-    wgbs.add_argument('-CHG','--mchg',  nargs='+', type=str, help= 'WGBS at CHG')
-    wgbs.add_argument('-CHH','--mchh',  nargs='+', type=str, help= 'WGBS at CHH')
-    
+    wgbs_group = parser.add_argument_group('甲基化计算参数')
+    wgbs_group.add_argument('-CG', '--mcg', nargs=2, type=str, help='CpG 位点的 WGBS 文件')
+    wgbs_group.add_argument('-CHG', '--mchg', nargs=2, type=str, help='CHG 位点的 WGBS 文件')
+    wgbs_group.add_argument('-CHH', '--mchh', nargs=2, type=str, help='CHH 位点的 WGBS 文件')
+
     return parser
 
-def  read_big_file(big_fileA, big_fileB):
+def read_big_file(file_a, file_b):
+    """
+    按行读取两个大文件，生成器形式返回每一行的数据。
 
+    参数：
+    - file_a, file_b: 文件对象
+    """
     while True:
-        data_chunk_from_fileA = big_fileA.readline()
-        data_chunk_from_fileB = big_fileB.readline()
-        if data_chunk_from_fileA == '' or data_chunk_from_fileB == '':
+        line_a = file_a.readline()
+        line_b = file_b.readline()
+        if not line_a or not line_b:
             break
-        
-        yield data_chunk_from_fileA.replace('\n',''),data_chunk_from_fileB.replace('\n','')
+        yield line_a.strip(), line_b.strip()
 
+def calc_mlevel(file_a_path, file_b_path):
+    """
+    计算甲基化水平。
 
-def calc_mlevel(fileA, fileB):
+    参数：
+    - file_a_path, file_b_path: 两个文件的路径
 
-    total, count, mismatch = 0, 0, 0
-    length = 900000000
-    # s_time = time.time()
-    with Pool(cpu_count-2) as pool:
-        with open(fileA, 'r') as big_fileA, open(fileB, 'r') as big_fileB:
-            for wgbs_from_fileA, wgbs_from_fileB in itertools.islice(read_big_file(big_fileA, big_fileB), length):
-                wgbs_from_fileA, wgbs_from_fileB = wgbs_from_fileA.split('\t'), wgbs_from_fileB.split('\t')
-                try:
-                    if int(wgbs_from_fileA[-2]) < 4 and int(wgbs_from_fileB[-2]) < 4 :
-                        continue
-                    elif (wgbs_from_fileA[-2]) == '0':
-                        total += int(wgbs_from_fileB[-1])/100 
-                        count += 1
-                    elif int(wgbs_from_fileB[-2]) == '0':   
-                        total += int(wgbs_from_fileA[-1])/100
-                        count += 1
-                    else:
-                        read_A = 4 if int(wgbs_from_fileA[-2]) >= 4 else int(wgbs_from_fileA[-2])
-                        read_B = 4 if int(wgbs_from_fileB[-2]) >= 4 else int(wgbs_from_fileB[-2])
-                        total += ((int(wgbs_from_fileA[-1])*read_A+ int(wgbs_from_fileB[-1])*read_B)/100) / (read_A+read_B)
-                        count += 1
-                except:
-                    pass
-        
-            pool.close()
-            pool.join()
-    
-    
-    # end =  time.time()
-    # print('花費時間：', (end - s_time)/60,'min') 
-    # print('m_prob:',total /count)
-    print('mismatch',mismatch)
-    return '{:.4f}'.format(total /count)
+    返回：
+    - total_mlevel: 总的甲基化水平，保留四位小数的字符串
+    """
+    total, count = 0, 0
+    max_length = 900_000_000  # 最大读取长度
 
+    with open(file_a_path, 'r') as file_a, open(file_b_path, 'r') as file_b:
+        for line_a, line_b in itertools.islice(read_big_file(file_a, file_b), max_length):
+            wgbs_a = line_a.split('\t')
+            wgbs_b = line_b.split('\t')
+            try:
+                reads_a = int(wgbs_a[-2])
+                reads_b = int(wgbs_b[-2])
+                methyl_a = int(wgbs_a[-1])
+                methyl_b = int(wgbs_b[-1])
 
-        
+                if reads_a < 4 and reads_b < 4:
+                    continue
+                elif reads_a == 0:
+                    total += methyl_b / 100
+                elif reads_b == 0:
+                    total += methyl_a / 100
+                else:
+                    reads_a = min(reads_a, 4)
+                    reads_b = min(reads_b, 4)
+                    weighted_methyl = ((methyl_a * reads_a + methyl_b * reads_b) / 100) / (reads_a + reads_b)
+                    total += weighted_methyl
+                count += 1
+            except (ValueError, IndexError):
+                continue
 
-def to_dict(lst, val):
-    res_dict = {lst[i]: val[i] for i in range(0, len(lst), 1)}
-    return res_dict 
+    if count == 0:
+        return '0.0000'
+    return f'{total / count:.4f}'
 
 def calc(seq):
-    
-    if seq.startswith('>') or (seq.startswith('N') and len(set(seq))==2):
-            return 0
+    """
+    计算给定序列的单核苷酸、二核苷酸频率以及 CpG、CHG、CHH 的计数。
 
-    
-    # monome_nucleobase_list = ['A', 'C', 'G', 'T', ]
-    # dimer_nucleobase_list = ['AA', 'AC', 'AG', 'AT', 'CA', 'CC', 'CG', 'CT', 'GA', 'GC', 'GG', 'GT', 'TA', 'TC', 'TG', 'TT']
-    
-    # 不cal to_dict會加速嗎?
-    # {'AA': 0, 'AC': 0, 'AG': 0, 'AT': 0, 'CA': 0, 'CC': 0, 'CG': 0, 'CT': 0, 'GA': 0, 'GC': 0, 'GG': 0, 'GT': 0, 'TA': 0, 'TC': 0, 'TG': 0, 'TT': 0}
-    # {'A': 0, 'C': 0, 'G': 0, 'T': 0}
-    dimer_nucleobase_dict = {'AA': 0, 'AC': 0, 'AG': 0, 'AT': 0, 'CA': 0, 'CC': 0, 'CG': 0, 'CT': 0, 'GA': 0, 'GC': 0, 'GG': 0, 'GT': 0, 'TA': 0, 'TC': 0, 'TG': 0, 'TT': 0}
-    monome_nucleobase_dict =  {'A': 0, 'C': 0, 'G': 0, 'T': 0}
-    cpg, chg, chh= 0, 0, 0
+    参数：
+    - seq: DNA 序列字符串
 
-    
-    for base in  range(0, len(seq), 1):
-        #calculate ['A', 'C', 'G', 'T', 'AA', 'AC' ... 'TG', 'TT']
-        if seq[base] in ["c", "C", "g", "G", "a", "A", "T", "t"]:
-            monome_nucleobase_dict[seq[base].upper()] += 1
-            if base  < len(seq)-1 and seq[base+1] in ["c", "C", "g", "G", "a", "A", "T", "t"]:
-                dimer = seq[base] + seq[base+1]
-                dimer_nucleobase_dict[dimer.upper()] += 1
-                
-        # calculate condition when given C/G
-        if  base >= 2 and base <= len(seq)-3:
-            
-            if seq[base] in ["c", "C"]:
-            
-                #CG condition | C base +
-                if seq[base+1] in ["g","G"] :
-                    cpg += 1
-                    
-                #CHG condition | C base +
-                elif seq[base+2] in ["g","G"]:
-                    chg += 1
-                    
-                #CHH condition | C base +   
+    返回：
+    - [cpg_count, chg_count, chh_count, mononucleotide_dict, dinucleotide_dict]
+    """
+    if seq.startswith('>') or (seq.startswith('N') and len(set(seq)) == 2):
+        return 0
+
+    dinucleotide_dict = {k: 0 for k in ['AA', 'AC', 'AG', 'AT', 'CA', 'CC', 'CG', 'CT',
+                                        'GA', 'GC', 'GG', 'GT', 'TA', 'TC', 'TG', 'TT']}
+    mononucleotide_dict = {k: 0 for k in ['A', 'C', 'G', 'T']}
+    cpg_count, chg_count, chh_count = 0, 0, 0
+
+    seq = seq.upper()
+    seq_len = len(seq)
+
+    for i in range(seq_len):
+        base = seq[i]
+        if base in mononucleotide_dict:
+            mononucleotide_dict[base] += 1
+
+            if i < seq_len - 1 and seq[i + 1] in mononucleotide_dict:
+                dinucleotide = base + seq[i + 1]
+                if dinucleotide in dinucleotide_dict:
+                    dinucleotide_dict[dinucleotide] += 1
+
+        if 2 <= i <= seq_len - 3:
+            if base == 'C':
+                if seq[i + 1] == 'G':
+                    cpg_count += 1
+                elif seq[i + 2] == 'G':
+                    chg_count += 1
                 else:
-                    chh += 1
-                    
-            elif seq[base] in ["G", "g"]:
-                
-                #CG condition | G base +
-                if seq[base-1] in ["c", "C"] :
-                    cpg += 1
-                    
-                #CHG condition | G base +
-                elif seq[base-2] in ["c", "C"]:
-                    chg += 1
-                    
-                #CHH condition | G base +
+                    chh_count += 1
+            elif base == 'G':
+                if seq[i - 1] == 'C':
+                    cpg_count += 1
+                elif seq[i - 2] == 'C':
+                    chg_count += 1
                 else:
-                    chh += 1 
-           
+                    chh_count += 1
 
+    return [cpg_count, chg_count, chh_count, mononucleotide_dict, dinucleotide_dict]
 
-    return [cpg, chg, chh, monome_nucleobase_dict, dimer_nucleobase_dict] 
-    # return [cpg, chg, chh, monome_nucleobase_dict, dimer_nucleobase_dict]                    
+def read_file(sequences):
+    """
+    读取序列文件，计算核苷酸和甲基化相关信息，并将结果保存为文件。
 
+    参数：
+    - sequences: 序列列表
+    """
+    start_time = time.time()
+    cpg_total, chg_total, chh_total = 0, 0, 0
+    mononucleotide_counts = np.zeros(4)
+    dinucleotide_counts = np.zeros(16)
 
-def read_file(file):    
-    filestime = time.time()
-    cpg, chg, chh =[0, 0, 0] 
+    with Pool(cpu_count()) as pool:
+        results = pool.map(calc, sequences)
 
-    monome, dimer = np.zeros(4), np.zeros(16)     
-
-    # stime = time.time()
-    # with open(file, 'r') as file:
-    #     file = file.readlines()
-    #     etime = time.time()            
-    #     print('read file finish cost ', (etime-stime)/60,' min \n' )
-
-    with Pool(cpu_count) as pool:
-            # res = pool.map(calc,[file[seq] for seq in range(len(file))])
-            res = pool.map(calc,file)
-                
-    for i in res:
-        if isinstance(i, int):
+    for result in results:
+        if isinstance(result, int):
             continue
+        cpg_total += result[0]
+        chg_total += result[1]
+        chh_total += result[2]
+        mononucleotide_counts += np.array(list(result[3].values()))
+        dinucleotide_counts += np.array(list(result[4].values()))
 
-        cpg += i[0]
-        chg += i[1]
-        chh += i[2]
+    total_cpg_chg_chh = cpg_total + chg_total + chh_total
+    if total_cpg_chg_chh == 0:
+        total_cpg_chg_chh = 1  # 防止除以零
 
-        monome += np.array(list(i[3].values()))
-        dimer += np.array(list(i[4].values()))
+    cpg_chg_chh_probs = [
+        f'{cpg_total / total_cpg_chg_chh:.4f}',
+        f'{chg_total / total_cpg_chg_chh:.4f}',
+        f'{chh_total / total_cpg_chg_chh:.4f}'
+    ]
 
-    sum = cpg + chh + chg
-    
-    cpg_chg_chh = ['{:.4f}'.format(cpg/sum), '{:.4f}'.format(chg/sum), '{:.4f}'.format(chh/sum)]
-    # print('CpG', cpg,round(cpg /sum,4))
-    # print('CHG',chg,round(chg /sum,4))
-    # print('CHH',chh,round(chh /sum,4))  
+    mono_total = mononucleotide_counts.sum()
+    if mono_total == 0:
+        mono_total = 1  # 防止除以零
 
-    # print('monome',monome.tolist())
-    # print('dimer',dimer.tolist())
-    sum =  reduce(lambda x,y : x + y, monome.tolist())
-    monome_prob = list(map(lambda x : round( x / sum, 4), monome.tolist()))
-    # print(monome_prob)
-    for monome in  monome_prob:
-       
-        cpg_chg_chh.append('{:.4f}'.format(monome))
+    mononucleotide_probs = mononucleotide_counts / mono_total
+    mononucleotide_probs = [f'{prob:.4f}' for prob in mononucleotide_probs]
 
+    cpg_chg_chh_probs.extend(mononucleotide_probs)
 
-    dimer_ = dimer.tolist()
-    m = []
+    dinucleotide_probs = dinucleotide_counts.copy()
     index = 0
-    for i in range(0, len(dimer_ ), 4):
-        temp = dimer_ [i : i + 4]
-        sum = reduce(lambda x, y : x + y, temp)
-        sum = monome_prob[index] / sum
-        for i in range(4):
-            m.append(sum)
+    for i in range(0, len(dinucleotide_counts), 4):
+        dinucleotide_group = dinucleotide_counts[i:i+4]
+        group_sum = dinucleotide_group.sum()
+        if group_sum == 0:
+            group_sum = 1  # 防止除以零
+        scale_factor = mononucleotide_counts[index] / group_sum
+        dinucleotide_probs[i:i+4] *= scale_factor
         index += 1
-    m = np.array(m)    
-    dimer_prob = np.multiply(dimer, m)
-    etime = time.time()  
-    # print(dimer_prob)
 
-    for dimer in  dimer_prob:
-        cpg_chg_chh.append('{:.4f}'.format(dimer))
+    dinucleotide_probs = [f'{prob:.4f}' for prob in dinucleotide_probs]
+    cpg_chg_chh_probs.extend(dinucleotide_probs)
 
-    print('calc finish..', (etime-filestime)/ 60,' min' ) 
-    df = pd.DataFrame(cpg_chg_chh, columns=['whole_genome'],index=[ 'CpG', 'CHG', 'CHH', 'A', 'C', 'G', 'T', 'AA', 'AC', 'AG', 'AT', 'CA', 'CC', 'CG', 'CT', 'GA', 'GC', 'GG', 'GT', 'TA', 'TC', 'TG', 'TT'])
-    # print(df)
-    path = dir_path + '/../Background_probability/whole_genome/'+ speciesname +'_whole_genome_probability.txt'
-    df.to_csv(path, sep='\t')
+    end_time = time.time()
+    print(f'计算完成，耗时 {(end_time - start_time) / 60:.2f} 分钟')
 
+    nucleotides = ['CpG', 'CHG', 'CHH', 'A', 'C', 'G', 'T']
+    dinucleotides = [
+        'AA', 'AC', 'AG', 'AT', 'CA', 'CC', 'CG', 'CT',
+        'GA', 'GC', 'GG', 'GT', 'TA', 'TC', 'TG', 'TT'
+    ]
+    index_labels = nucleotides + dinucleotides
 
+    df = pd.DataFrame(cpg_chg_chh_probs, columns=['whole_genome'], index=index_labels)
+    output_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        '..',
+        'Background_probability',
+        'whole_genome',
+        f'{speciesname}_whole_genome_probability.txt'
+    )
+    df.to_csv(output_path, sep='\t')
+    print(f'结果已保存到 {output_path}')
 
-def  main():
-    
-    global cpu_count, dir_path, speciesname, wgbs_file, celltype
-    cpu_count = mp.cpu_count()
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+def main():
+    global cpu_count, speciesname, wgbs_file, celltype
 
     parser = get_parser()
     args = parser.parse_args()
-  
-    species_fa= dir_path + args.fasta
+
+    speciesname = args.speciesname
     celltype = args.celltype
-    speciesname  =  args.speciesname
     wgbs_file = [args.mcg, args.mchg, args.mchh]
+    fasta_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), args.fasta)
 
-    print('\n')
-    print('File:',species_fa)
-    print('The computational will take approximately 10 minutes.\n')
-    stime = time.time()  
-    with open(species_fa, 'r') as file:
-        file = file.readlines()
-        etime = time.time()            
-        print('read file finish cost ', (etime-stime)/60,' min \n' )
-        read_file(file)
-        print('\n')
+    print(f'\n文件路径：{fasta_path}')
+    print('计算大约需要 10 分钟时间。\n')
 
-    if wgbs_file[0] != None:
-        print('start calculating mlevel...')
+    start_time = time.time()
+    with open(fasta_path, 'r') as fasta_file:
+        sequences = fasta_file.readlines()
+    end_time = time.time()
+    print(f'读取文件完成，耗时 {(end_time - start_time) / 60:.2f} 分钟\n')
 
+    read_file(sequences)
 
-        start = time.time()
-        CHG_result = calc_mlevel(wgbs_file[1][0], wgbs_file[1][1])
-        end = time.time()
-        print('CHG file finish cost ', (end-start)/60,' min \n' ) 
+    if wgbs_file[0]:
+        print('开始计算甲基化水平...\n')
 
-        start = time.time()
-        CHH_result = calc_mlevel(wgbs_file[2][0], wgbs_file[2][1])
-        end = time.time()
-        print('CHH file finish cost ', (end-start)/60,' min \n' )   
+        for label, files in zip(['CHG', 'CHH', 'CG'], wgbs_file[1:] + [wgbs_file[0]]):
+            if files and len(files) == 2:
+                start = time.time()
+                result = calc_mlevel(files[0], files[1])
+                end = time.time()
+                print(f'{label} 文件计算完成，耗时 {(end - start) / 60:.2f} 分钟\n')
+                wgbs_result[label] = result
+            else:
+                wgbs_result[label] = '0.0000'
 
-        start = time.time()
-        CG_result = calc_mlevel(wgbs_file[0][0], wgbs_file[0][1])
-        end = time.time()
-        print('CG file finish cost ', (end-start)/60,' min \n' )   
-
-
-
-        df = pd.DataFrame([CG_result, CHG_result, CHH_result], columns=['whole_genome'],index=['mCG','mCHG', 'mCHH' ])
-        path = dir_path + '/../Background_probability/whole_genome/'+ speciesname + '_'+ celltype +'_whole_genome_methyl_probability.txt'
-        df.to_csv(path, sep='\t', float_format = '%.4f')
-
-
+        df = pd.DataFrame.from_dict(wgbs_result, orient='index', columns=['whole_genome'])
+        output_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            '..',
+            'Background_probability',
+            'whole_genome',
+            f'{speciesname}_{celltype}_whole_genome_methyl_probability.txt'
+        )
+        df.to_csv(output_path, sep='\t', float_format='%.4f')
+        print(f'甲基化水平结果已保存到 {output_path}')
 
 if __name__ == '__main__':
+    wgbs_result = {}
     main()
